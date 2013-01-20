0886c4bc11f4aa459c12bdaa2d12655cd3712d07
Christopher Wellons <mosquitopsu@gmail.com>
2013-01-20 16:31:24 -0500

    Initial commit of working stuff.

---
+++ README.md
@@ -0,0 +1,9 @@
+# YouTube Playlist Fetcher
+
+Generates a shell script to download an entire playlist in parallel
+using [youtube-dl](http://rg3.github.com/youtube-dl/) and xargs.
+
+Use it here: http://skeeto.github.com/yt-playlist-fetch/
+
+This would be a command-line program except that sifting XML through
+anything but browser (DOM) JavaScript is unbearable.
---
+++ yt-playlist-fetch.js
@@ -0,0 +1,43 @@
+function escapeHTML(text) {
+    return $('<div/>').text(text).html();
+}
+
+$('#download').hide();
+function viewPlaylist(playlist) {
+    var api = 'http://gdata.youtube.com/feeds/api/playlists/';
+    var any = 'http://anyorigin.com/get?callback=?&url=';
+    $.getJSON(any + encodeURIComponent(api + playlist), function(json) {
+        var $script = $('#script');
+        var $xml = $($.parseXML(json.contents));
+        var title = $xml.find('title').first().text();
+        var name = $xml.find('name').first().text();
+        $script.text('#!/bin/bash\n\n')
+            .append('# ' + escapeHTML(title))
+            .append(' (' + playlist + ')\n')
+            .append('# ' + escapeHTML(name) + '\n\n')
+            .append('xargs -n1 -P4 youtube-dl -t &lt;&lt;EOF\n');
+        $xml.find('player').each(function() {
+            $script.append($(this).attr('url').match(/(^.+)&/)[1] + '\n');
+        });
+        $script.append('EOF');
+        var uri = "data:application/octet-stream," +
+                encodeURIComponent($script.text());
+        $('#download').attr('href', uri).show();
+    });
+}
+
+var playlist = location.hash.slice(1);
+$('#playlist').val(playlist);
+
+$('form').bind('submit', function() {
+    var playlist = $('#playlist').val();
+    location.hash = playlist;
+    viewPlaylist(playlist);
+    return false;
+});
+
+if (playlist) {
+    viewPlaylist(playlist);
+} else {
+    $('#script').append('# Select a playlist.');
+}
